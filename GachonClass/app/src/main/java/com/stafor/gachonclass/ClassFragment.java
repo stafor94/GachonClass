082c5350019b377c720a729bdfa16d07af7f54fd
Hwang <stafor@naver.com>
2017-05-22 10:12:00 +0900

    강의실 건물 선택 버튼 수정

--- GachonClass/app/src/main/java/com/stafor/gachonclass/ClassFragment.java
+++ GachonClass/app/src/main/java/com/stafor/gachonclass/ClassFragment.java
@@ -7,10 +7,14 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class ClassFragment extends Fragment {
+    LinearLayout layout;
     ImageView imageView;
+    Button[] buttons = new Button[11];
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -25,6 +29,24 @@ public class ClassFragment extends Fragment {
                 startActivity(myIntent);
             }
         });
+
+        layout = (LinearLayout) rootView.findViewById(R.id.builds);
+        for (int i = 0; i < 11; i++) {
+            buttons[i] = new Button(getContext());
+            layout.addView(buttons[i]);
+        }
+        buttons[0].setText("가천관");
+        buttons[1].setText("비전타워");
+        buttons[2].setText("법과대학");
+        buttons[3].setText("공과대학1");
+        buttons[4].setText("공과대학2");
+        buttons[5].setText("바이오나노대학");
+        buttons[6].setText("IT대학");
+        buttons[7].setText("한의과대학");
+        buttons[8].setText("예술대학1");
+        buttons[9].setText("예술대학2");
+        buttons[10].setText("글로벌센터");
+
         return rootView;
     }
 }
